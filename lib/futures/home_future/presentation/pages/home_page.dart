6ec9d03f372220db8b89fcbeda616d893e80d2cd
Amin <helloamin.com@gmail.com>
2025-12-27 20:34:47 +0330

    AppBar created.

--- lib/futures/home_future/presentation/pages/home_page.dart
+++ lib/futures/home_future/presentation/pages/home_page.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:personal_page/core/constants/colors.dart';
+import 'package:personal_page/core/utils/theme_extension.dart';
+import 'package:personal_page/futures/home_future/presentation/widgets/app_bar_actions.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      //* app bar of page
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: Icon(Icons.flutter_dash_rounded),
+        actions: [AppBarActions()],
+      ),
+
+      //* body of page
+      body: SizedBox.expand(child: Column(children: [
+            
+          ],
+        )),
+    );
   }
 }
---
+++ lib/futures/home_future/presentation/widgets/app_bar_actions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppBarActions extends StatelessWidget {
+  const AppBarActions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed: () {}, icon: Icon(Icons.menu_rounded));
+  }
+}
